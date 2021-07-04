a4c1dfab1720e26e76e435ae9d4d7a55d398075e
jordan <user@DESKTOP-DIAKH25.localdomain>
2021-07-04 17:25:27 +0900

    [공부] spring-cloud-netflix-eureka-server 기본예제

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Spring Cloud Config Server 기본
 
----
-
 spring cloud server 관련 예제
 
 
@@ -13,4 +11,14 @@ spring cloud server 관련 예제
 문제 해결
 1. resources/application.yml - No such label : master 문제  
    https://stackoverflow.com/questions/40024161/spring-config-server-no-such-label-master
- 
+
+
+---
+
+# Spring Cloud Netflix Eureka Server
+
+Eureka Server를 구축하는 예제
+
+git commit을 참고
+
+출처 : https://mr-spock.tistory.com/44
--- src/main/java/com/example/eureka/EurekaApplication.java
+++ src/main/java/com/example/eureka/EurekaApplication.java
@@ -3,9 +3,10 @@ package com.example.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
